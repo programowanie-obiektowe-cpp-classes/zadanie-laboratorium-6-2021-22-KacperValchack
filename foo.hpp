6c920e18287c5e70a67f029aa5729abcf3896f25
Kacper Walczak <01132936@pw.edu.pl>
2022-01-13 17:34:41 +0100

    moja implementacja

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,8 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
     // Twoja implementacja tutaj
-
+    std::for_each(begin(people), end(people), [](Human& man) { man.birthday(); });
+    std::transform(begin(people), end(people), rbegin(ret_v), [](Human& man) { return man.isMonster() ? 'n' : 'y';});
     return ret_v;
 }
